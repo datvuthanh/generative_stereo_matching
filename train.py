87db49bc9acffecebd572e9f8ea404d3948a03ae
Thanh Dat Vu <44630913+datvuthanh@users.noreply.github.com>
2021-01-12 13:53:38 +0700

    Update train.py

--- train.py
+++ train.py
@@ -12,6 +12,139 @@ import tensorflow_addons as tfa
 import tensorflow.keras.backend as K 
 import datetime
 
+# AUGEMNTATION
+def load(image_file):
+  left_pos,right_pos,left_neg,right_neg = tf.split(image_file,4,axis=0)
+
+  left_pos = tf.squeeze(left_pos,axis=0)
+  right_pos = tf.squeeze(right_pos,axis=0)
+  left_neg = tf.squeeze(left_neg,axis=0)
+  right_neg = tf.squeeze(right_neg,axis=0)
+
+  print("One patch shape: ", left_pos.shape)
+
+
+  left_pos = tf.cast(left_pos, tf.float32)
+  right_pos = tf.cast(right_pos, tf.float32)
+  left_neg = tf.cast(left_neg, tf.float32)
+  right_neg = tf.cast(right_neg, tf.float32)
+  
+  return left_pos,right_pos,left_neg,right_neg
+
+def resize(left_pos,right_pos,left_neg,right_neg, height, width):
+  left_pos = tf.image.resize(left_pos, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  right_pos = tf.image.resize(right_pos, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  left_neg = tf.image.resize(left_neg, [height, width],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  right_neg = tf.image.resize(right_neg, [height, width],
+                               method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+
+  return left_pos,right_pos,left_neg,right_neg
+
+def random_crop(left_pos,right_pos,left_neg,right_neg):
+  IMG_HEIGHT = 32
+  IMG_WIDTH = 32
+  stacked_image = tf.stack([left_pos,right_pos,left_neg,right_neg], axis=0)
+  print("STACKED IMAGE: ",stacked_image.shape)
+  cropped_image = tf.image.random_crop(
+      stacked_image, size=[4, IMG_HEIGHT, IMG_WIDTH, 3])
+
+  return cropped_image[0], cropped_image[1],cropped_image[2], cropped_image[3]
+
+@tf.function()
+def random_jitter(left_pos,right_pos,left_neg,right_neg):
+  # resizing to 62 x 62 x 3
+  left_pos,right_pos,left_neg,right_neg = resize(left_pos,right_pos,left_neg,right_neg, 62, 62)
+
+  # randomly cropping to 256 x 256 x 3
+  left_pos,right_pos,left_neg,right_neg = random_crop(left_pos,right_pos,left_neg,right_neg)
+
+  if tf.random.uniform(()) > 0.5:
+    # random mirroring
+    left_pos = tf.image.flip_left_right(left_pos)
+    right_pos = tf.image.flip_left_right(right_pos)
+    left_neg = tf.image.flip_left_right(left_neg)
+    right_neg = tf.image.flip_left_right(right_neg)
+
+  # # flip_up_down
+  # if tf.random.uniform(()) > 0.5:
+  #     input_l = tf.image.flip_up_down(input_l)
+  #     input_r = tf.image.flip_up_down(input_r)
+  #     target_l = tf.image.flip_up_down(target_l)
+  #     target_r = tf.image.flip_up_down(target_r)
+
+  # # brighness change
+  # if tf.random.uniform(()) > 0.5:
+  #     rand_value = tf.random.uniform((), minval=-5.0, maxval=5.0)
+  #     input_l = input_l + rand_value
+
+  #     rand_value = tf.random.uniform((), minval=-5.0, maxval=5.0)
+  #     input_r = input_r + rand_value
+
+  #     rand_value = tf.random.uniform((), minval=-5.0, maxval=5.0)
+  #     target_l = target_l + rand_value
+
+  #     rand_value = tf.random.uniform((), minval=-5.0, maxval=5.0)
+  #     target_r = target_r + rand_value
+
+  # # contrast change
+  # if tf.random.uniform(()) > 0.5:
+  #     rand_value = tf.random.uniform((), minval=0.8, maxval=1.2)
+  #     mean_value = tf.reduce_mean(input_l)
+  #     input_l = (input_l - mean_value) * rand_value + mean_value
+
+  #     rand_value = tf.random.uniform((), minval=0.8, maxval=1.2)
+  #     mean_value = tf.reduce_mean(input_r)
+  #     input_r = (input_r - mean_value) * rand_value + mean_value
+
+  #     rand_value = tf.random.uniform((), minval=0.8, maxval=1.2)
+  #     mean_value = tf.reduce_mean(target_l)
+  #     target_l = (target_l - mean_value) * rand_value + mean_value
+
+  #     rand_value = tf.random.uniform((), minval=0.8, maxval=1.2)
+  #     mean_value = tf.reduce_mean(target_r)
+  #     target_r = (target_r - mean_value) * rand_value + mean_value
+
+  # # clip value
+  left_pos = tf.clip_by_value(left_pos, clip_value_min=0.0, clip_value_max=255.0)
+  right_pos = tf.clip_by_value(right_pos, clip_value_min=0.0, clip_value_max=255.0)
+  left_neg = tf.clip_by_value(left_neg, clip_value_min=0.0, clip_value_max=255.0)
+  right_neg = tf.clip_by_value(right_neg, clip_value_min=0.0, clip_value_max=255.0)
+
+  # # rotate positive samples for making hard positive cases
+  # if tf.random.uniform(()) > 0.5:
+  #     if tf.random.uniform(()) < 0.5:
+  #         input_l = tfa.image.rotate(input_l, 1.5707963268)  # 90
+  #         input_r = tfa.image.rotate(input_r, 1.570796326)  # 90
+  #     else:
+  #         input_l = tfa.image.rotate(input_l, 4.7123889804)  # 270
+  #         input_r = tfa.image.rotate(input_r, 4.7123889804)  # 270
+
+
+  return left_pos,right_pos,left_neg,right_neg
+  
+# normalizing the images to [0, 1]
+
+def normalize(left_pos,right_pos,left_neg,right_neg):
+
+  # Normalize 
+  left_pos = left_pos / 255.0 #(left_pos - left_pos.mean()) / left_pos.std()
+  right_pos = right_pos / 255.0 #(right_pos - right_pos.mean()) / right_pos.std()
+  left_neg = left_neg / 255.0 #(left_neg - left_neg.mean()) / left_neg.std()
+  right_neg = right_neg / 255.0 #(right_neg - right_neg.mean()) / right_neg.std()
+
+  return left_pos,right_pos,left_neg,right_neg
+
+def load_image_train(image_file):
+  left_pos,right_pos,left_neg,right_neg = load(image_file)
+  left_pos,right_pos,left_neg,right_neg = random_jitter(left_pos,right_pos,left_neg,right_neg)
+  left_pos,right_pos,left_neg,right_neg = normalize(left_pos,right_pos,left_neg,right_neg)
+  
+  stacked_image = tf.stack([left_pos,right_pos,left_neg,right_neg], axis=0)
+  return stacked_image
+
 
 def extract_first_features(filters, size, strides, apply_batchnorm=True):
   initializer = tf.keras.initializers.he_normal(seed=None)
@@ -110,22 +243,19 @@ def train_step(left_patch_positive, right_patch_positive,left_patch_negative,rig
 
     return total_loss,loss_pos,loss_neg,pos_output,neg_output
 if __name__ == '__main__':
-    EPOCHS = 20    
-    disp_range = 201 # For kitti 2015
+    EPOCHS = 50    
     log_dir = "logs/"
     
     summary_writer = tf.summary.create_file_writer(log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
     images = np.load('../generation/images.npy', allow_pickle=True)  
-
-    # Normalize 
-    images = (images - images.mean()) / images.std()
     #print(images[0][0]) # Test for normalization
     print("DATASET SHAPE: ",images.shape)
     BUFFER_SIZE = images.shape[0]
     BATCH_SIZE = 256
 
     train_dataset = tf.data.Dataset.from_tensor_slices(images)
+    train_dataset = train_dataset.map(load_image_train)#num_parallel_calls=tf.data.AUTOTUNE)
     train_dataset = train_dataset.shuffle(BUFFER_SIZE)
     train_dataset = train_dataset.batch(BATCH_SIZE)
 
@@ -200,7 +330,7 @@ if __name__ == '__main__':
         average_negl = average_negl / count
 
         print('epoch {}  average_loss {}'.format(epoch, average_loss))
-        print('normal loss {}  perceptual loss {}  '.format(average_posl, average_negl))
+        print('pos loss {}  neg loss {}  '.format(average_posl, average_negl))
 
         pos_acc = (count_ones_pos * 100.0) / BUFFER_SIZE
         neg_acc = (count_ones_neg * 100.0) / BUFFER_SIZE
@@ -215,5 +345,3 @@ if __name__ == '__main__':
         save_path = manager.save()
         print("\n----------Saved checkpoint for epoch {}: {}-----------\n".format(epoch+1, save_path))
         print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,time.time()-start))
-
-    
